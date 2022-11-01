60ce94556a69621a14d7d474c6fdf08a21d9a558
Justin Russo <j.russo@outlook.com>
2022-11-01 01:34:47 -0400

    chore(repo): set up a ton of dev features

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  root: true,
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:prettier/recommended",
+  ],
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint"],
+  env: {
+    es2021: true,
+    node: true,
+  },
+};
