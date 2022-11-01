02d2de8d420b58c0b266b0fce611d4b12786e470
Justin Russo <j.russo@outlook.com>
2022-11-01 02:47:12 -0400

    chore(repo): set up toml and json linting

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,36 @@
+/** @type {import('eslint').Linter.Config} */
 module.exports = {
   root: true,
-  extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:prettier/recommended",
-  ],
-  parser: "@typescript-eslint/parser",
-  plugins: ["@typescript-eslint"],
+  extends: ["eslint:recommended", "plugin:editorconfig/all"],
+  plugins: ["@typescript-eslint", "editorconfig"],
   env: {
     es2021: true,
     node: true,
   },
+  overrides: [
+    {
+      files: "*.toml",
+      parser: "toml-eslint-parser",
+      extends: ["plugin:toml/recommended"],
+    },
+    {
+      files: "*.json",
+      extends: ["plugin:json/recommended", "plugin:prettier/recommended"],
+    },
+    {
+      files: "tsconfig.json",
+      rules: {
+        "json/*": ["error", { allowComments: true }],
+      },
+    },
+    {
+      files: ["*.js", "*.ts"],
+      extends: ["plugin:prettier/recommended"],
+    },
+    {
+      files: "*.ts",
+      extends: ["plugin:@typescript-eslint/recommended"],
+      parser: "@typescript-eslint/parser",
+    },
+  ],
 };
