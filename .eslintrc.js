08e07e65f2fe79cfcd145a0739cda76f6f573d24
Justin Russo <j.russo@outlook.com>
2022-11-01 23:23:45 -0400

    chore(repo): improve json and toml linting

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,28 @@
 /** @type {import('eslint').Linter.Config} */
-module.exports = {
+const config = {
   root: true,
-  extends: ["eslint:recommended", "plugin:editorconfig/all"],
+  extends: [
+    "eslint:recommended",
+    "plugin:toml/recommended",
+    "plugin:jsonc/prettier",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:editorconfig/all",
+    "plugin:prettier/recommended",
+  ],
   plugins: ["@typescript-eslint", "editorconfig"],
+  rules: {
+    "toml/indent": ["error", 2, { subTables: 1, keyValuePairs: 1 }],
+    "toml/no-space-dots": "error",
+    "toml/padding-line-between-tables": "error",
+    "toml/quoted-keys": "error",
+    "toml/array-bracket-newline": "error",
+    "toml/array-element-newline": ["error", "consistent"],
+    "toml/comma-style": "error",
+    "toml/inline-table-curly-spacing": "error",
+    "toml/key-spacing": "error",
+    "toml/spaced-comment": ["error", "always"],
+    "toml/table-bracket-spacing": ["error", "always"],
+  },
   env: {
     es2021: true,
     node: true,
@@ -11,26 +31,19 @@ module.exports = {
     {
       files: "*.toml",
       parser: "toml-eslint-parser",
-      extends: ["plugin:toml/recommended"],
-    },
-    {
-      files: "*.json",
-      extends: ["plugin:json/recommended", "plugin:prettier/recommended"],
-    },
-    {
-      files: "tsconfig.json",
       rules: {
-        "json/*": ["error", { allowComments: true }],
+        "prettier/prettier": "off",
       },
     },
     {
-      files: ["*.js", "*.ts"],
-      extends: ["plugin:prettier/recommended"],
+      files: ["*.json", "*.json5", "*.jsonc", "*.mcmeta"],
+      parser: "jsonc-eslint-parser",
     },
     {
       files: "*.ts",
-      extends: ["plugin:@typescript-eslint/recommended"],
       parser: "@typescript-eslint/parser",
     },
   ],
 };
+
+module.exports = config;
