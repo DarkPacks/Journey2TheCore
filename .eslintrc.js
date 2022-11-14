8be6798271c85b33546b1dcfff19341ac583b084
Justin Russo <j.russo@outlook.com>
2022-11-14 18:22:43 -0500

    chore(repo): add schema verification for incontrol configs

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,11 @@
 /** @type {import('eslint').Linter.Config} */
 const config = {
   root: true,
-  extends: ["./configs/eslint/common", "./configs/eslint/minecraft"],
+  extends: [
+    "./configs/eslint/common",
+    "./configs/eslint/json-schema",
+    "./configs/eslint/minecraft",
+  ],
 };
 
 module.exports = config;
